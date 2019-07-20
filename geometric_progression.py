1321122909795dbd4a9a9431603245bec643e76e
Arafat Khan <arafat33k@outlook.com>
2019-07-20 15:41:47 +0600

    added ability to find the value of given nth term

--- geometric_progression.py
+++ geometric_progression.py
@@ -1,5 +1,11 @@
-arithmetic_progression.py
-geometric_progression.py
-arithmetic_progression.py
-geometric_progression.py
-[H[2J[3J
+print("This is a geometric progression program")
+
+user_choice = input("a) Find the value of a given nth term: ")
+
+if user_choice == "a":
+    first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")
+    first_term, second_term = int(first_term), int(second_term)
+    comman_ratio = second_term / first_term
+    user_nth_term = int(input("Now, enter the term you want the value of: "))
+    value_nth_term = first_term * (comman_ratio ** (user_nth_term-1))
+    print(value_nth_term)

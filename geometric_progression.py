a2a48873f507a3b13644845e22f3866bad932111
Arafat Khan <arafat33k@outlook.com>
2019-07-20 20:07:43 +0600

    Add function to convert float answers to integer

--- geometric_progression.py
+++ geometric_progression.py
@@ -4,17 +4,29 @@ print("This is a geometric progression program")
 user_choice = input(
     "a) Find the value of a given nth term \nb) Find the term of a given value \nc) Find the first term and common ratio with other given values \nd) Find the sum of a geometric series \ne) Find the sum to infinity of a geometric series \n")
 
+def float_to_int_converter(float_number):
+    if type(float_number) == float:
+        if str(float_number)[-1] == "0":
+            return int(float_number)
+        else:
+            return float_number
+    else:
+        return "The number given is not a float"
+
+
 def find_value(first_term, second_term):
     comman_ratio = second_term / first_term
     user_nth_term = int(input("Now, enter the term you want the value of: "))
     value_of_nth_term = first_term * (comman_ratio ** (user_nth_term-1))
-    print( value_of_nth_term )
+    answer = float_to_int_converter(value_of_nth_term)
+    print(answer)
 
 def find_term(first_term, second_term):
     comman_ratio = second_term / first_term
     user_term_value = int(input("Enter the value of the term you want: "))
     term_of_value = (log(user_term_value/first_term)/(log(comman_ratio))) + 1
-    print(term_of_value)
+    answer = float_to_int_converter(term_of_value)
+    print(answer)
 
 def find_first_term_and_ratio():
     any_term1 = int(input("Enter the term "))
@@ -41,7 +53,8 @@ def find_sum(first_term, second_term):
         sumation = (first_term * (1-(comman_ratio ** number_of_terms))) / (comman_ratio-1)
     elif comman_ratio > 1:
         sumation = (first_term * ((comman_ratio ** number_of_terms)-1)) / (comman_ratio-1)
-    print(sumation)
+    answer = float_to_int_converter(sumation)
+    print(answer)
 
 def find_sum_to_infinity(first_term, second_term):
     comman_ratio = second_term / first_term
@@ -49,7 +62,8 @@ def find_sum_to_infinity(first_term, second_term):
         print("The sum of infinity series does not exist")
     elif comman_ratio < 1 and comman_ratio > -1:
         sum_to_infinity = first_term / (1-comman_ratio)
-    print(int(sum_to_infinity))
+    answer = float_to_int_converter(sum_to_infinity)
+    print(answer)
 
 
 first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")

e9daff271a478fc9be3e8b497874f03c319f3bb3
Arafat Khan <arafat33k@outlook.com>
2019-07-20 15:52:45 +0600

    find the term of a given value

--- geometric_progression.py
+++ geometric_progression.py
@@ -1,11 +1,20 @@
+from math import log
 print("This is a geometric progression program")
 
-user_choice = input("a) Find the value of a given nth term: ")
+user_choice = input("a) Find the value of a given nth term: \nb) Find the term of a given value")
 
 if user_choice == "a":
     first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")
     first_term, second_term = int(first_term), int(second_term)
     comman_ratio = second_term / first_term
     user_nth_term = int(input("Now, enter the term you want the value of: "))
-    value_nth_term = first_term * (comman_ratio ** (user_nth_term-1))
-    print(value_nth_term)
+    value_of_nth_term = first_term * (comman_ratio ** (user_nth_term-1))
+    print(value_of_nth_term)
+
+if user_choice == "b":
+    first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")
+    first_term, second_term = int(first_term), int(second_term)
+    comman_ratio = second_term / first_term
+    user_term_value = int(input("Enter the value of the term you want: "))
+    term_of_value = (log(user_term_value/first_term)/(log(comman_ratio))) + 1
+    print(term_of_value)

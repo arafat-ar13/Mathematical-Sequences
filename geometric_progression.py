e8971956c3fed02c8b479a9ef7d12e5643fb279f
Arafat Khan <arafat33k@outlook.com>
2019-07-20 19:50:54 +0600

    added functions to avoid code repitition

--- geometric_progression.py
+++ geometric_progression.py
@@ -2,26 +2,21 @@ from math import log
 print("This is a geometric progression program")
 
 user_choice = input(
-    "a) Find the value of a given nth term \nb) Find the term of a given value \nc) Find the first term and common ratio with other given values \nd) Find the sum of a geometric series \ne) Find the sum to infinity of a geometric series")
+    "a) Find the value of a given nth term \nb) Find the term of a given value \nc) Find the first term and common ratio with other given values \nd) Find the sum of a geometric series \ne) Find the sum to infinity of a geometric series \n")
 
-if user_choice == "a":
-    first_term, second_term = input(
-        "Enter the first and second terms, separated by commas: ").split(",")
-    first_term, second_term = int(first_term), int(second_term)
+def find_value(first_term, second_term):
     comman_ratio = second_term / first_term
     user_nth_term = int(input("Now, enter the term you want the value of: "))
     value_of_nth_term = first_term * (comman_ratio ** (user_nth_term-1))
-    print(value_of_nth_term)
+    print( value_of_nth_term )
 
-if user_choice == "b":
-    first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")
-    first_term, second_term = int(first_term), int(second_term)
+def find_term(first_term, second_term):
     comman_ratio = second_term / first_term
     user_term_value = int(input("Enter the value of the term you want: "))
     term_of_value = (log(user_term_value/first_term)/(log(comman_ratio))) + 1
     print(term_of_value)
 
-if user_choice == "c":
+def find_first_term_and_ratio():
     any_term1 = int(input("Enter the term "))
     any_term1_value = int(input("Enter its value: "))
 
@@ -39,9 +34,7 @@ if user_choice == "c":
     print("First term:" + str(a))
     print("Ratio:" + str(r))
 
-if user_choice == "d":
-    first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")
-    first_term, second_term = int(first_term), int(second_term)
+def find_sum(first_term, second_term):
     comman_ratio = second_term / first_term
     number_of_terms = int(input("How many terms in this series?: "))
     if comman_ratio < 1:
@@ -50,13 +43,25 @@ if user_choice == "d":
         sumation = (first_term * ((comman_ratio ** number_of_terms)-1)) / (comman_ratio-1)
     print(sumation)
 
-if user_choice == "e":
-    first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")
-    first_term, second_term = int(first_term), int(second_term)
+def find_sum_to_infinity(first_term, second_term):
     comman_ratio = second_term / first_term
     if comman_ratio > 1:
         print("The sum of infinity series does not exist")
     elif comman_ratio < 1 and comman_ratio > -1:
-        for n in range(100):
-            sum_to_infinity = first_term / (1-comman_ratio)
+        sum_to_infinity = first_term / (1-comman_ratio)
     print(int(sum_to_infinity))
+
+
+first_term, second_term = input("Enter the first and second terms, separated by commas: ").split(",")
+first_term, second_term = int(first_term), int(second_term)
+
+if user_choice == "a":
+    find_value(first_term, second_term)
+elif user_choice == "b":
+    find_term(first_term, second_term)
+elif user_choice == "c":
+    find_first_term_and_ratio()
+elif user_choice == "d":
+    find_sum(first_term, second_term)
+elif user_choice == "e":
+    find_sum_to_infinity(first_term, second_term)

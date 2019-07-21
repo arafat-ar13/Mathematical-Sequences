1ebecf8455b6dff61d170a6ab2d6e6f467a27ce4
Arafat Khan <arafat33k@outlook.com>
2019-07-21 14:10:50 +0000

    Find the term of a given value

--- arithmetic_progression.py
+++ arithmetic_progression.py
@@ -1,11 +1,22 @@
 print("This program simulates an arithmetic progression")
 
-user_choice = input("a) Find the value of a given term")
+user_choice = input("a) Find the value of a given term \nb) Find the term of a given value \n" )
 
-if user_choice == "a":
-    first_term, second_term = input("Enter the first and second term, separated by commas ").split(",")
-    first_term, second_term = int(first_term), int(second_term)
-    common_difference = second_term - first_term
+def find_value(first_term, second_term, common_difference):
     user_nth_term = int(input("Enter the term you want the value of: "))
     value_of_nth_term = (first_term) + ((user_nth_term-1) * (common_difference))
     print(f"The value of {user_nth_term}th/nd/st of this series is {value_of_nth_term}")
+
+def find_term(first_term, second_term, common_difference):
+    user_value_of_nth_term = int(input("Enter the value of the term you are looking for: "))
+    term_of_value = ((user_value_of_nth_term + common_difference)-first_term) / common_difference
+    print(f"The value {user_value_of_nth_term} belongs to the term {term_of_value} for this series")
+
+first_term, second_term = input("Enter the first and second term, separated by commas ").split(",")
+first_term, second_term = int(first_term), int(second_term)
+common_difference = second_term - first_term
+
+if user_choice == "a":
+    find_value(first_term, second_term, common_difference)
+elif user_choice == "b":
+    find_term(first_term, second_term, common_difference)

eb9d12abca6aede15b2b276149881588ed291320
Arafat Khan <arafat33k@outlook.com>
2019-07-22 00:21:50 +0600

    Find sum of a series

--- arithmetic_progression.py
+++ arithmetic_progression.py
@@ -1,24 +1,41 @@
 from float_to_int import float_to_int_converter
+from time import sleep
 print("This program simulates an arithmetic progression")
 
-user_choice = input("a) Find the value of a given term \nb) Find the term of a given value \n" )
+sleep(0.5)
+user_choice = input("a) Find the value of a given term \nb) Find the term of a given value \nc) Find the sum of a series \n" )
 
 def find_value(first_term, second_term, common_difference):
     user_nth_term = int(input("Enter the term you want the value of: "))
     value_of_nth_term = (first_term) + ((user_nth_term-1) * (common_difference))
+    sleep(0.75)
     print(f"The value of {user_nth_term}th/nd/st of this series is {value_of_nth_term}")
 
 def find_term(first_term, second_term, common_difference):
     user_value_of_nth_term = int(input("Enter the value of the term you are looking for: "))
     term_of_value = ((user_value_of_nth_term + common_difference)-first_term) / common_difference
     answer = float_to_int_converter(term_of_value)
+    sleep(0.75)
     print(f"The value {user_value_of_nth_term} belongs to the term {answer} for this series")
 
+def find_sum(first_term, second_term, common_difference):
+    how_many_terms = int(input("How many terms are in this series?: "))
+    sum_of_series = (how_many_terms/2) * ((2*first_term) + ((how_many_terms-1) * common_difference))
+    answer = float_to_int_converter(sum_of_series)
+    sleep(0.75)
+    print(f"The sum of this series is {answer}")
+
+sleep(0.75)
 first_term, second_term = input("Enter the first and second term, separated by commas ").split(",")
 first_term, second_term = int(first_term), int(second_term)
 common_difference = second_term - first_term
 
 if user_choice == "a":
+    sleep(0.75)
     find_value(first_term, second_term, common_difference)
 elif user_choice == "b":
+    sleep(0.75)
     find_term(first_term, second_term, common_difference)
+elif user_choice == "c":
+    sleep(0.75)
+    find_sum(first_term, second_term, common_difference)

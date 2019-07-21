eb69ac85219e11f83b2114996d18165cc774e0c0
Arafat Khan <arafat33k@outlook.com>
2019-07-21 11:40:49 +0600

    Add messages for better answers

--- geometric_progression.py
+++ geometric_progression.py
@@ -19,15 +19,17 @@ def float_to_int_converter(float_number):
 
 def find_value(first_term, second_term, common_ratio):
     user_nth_term = int(input("Now, enter the term you want the value of: "))
-    value_of_nth_term = first_term * (common_ratio ** (user_nth_term-1))
-    answer = float_to_int_converter(value_of_nth_term)
-    print(answer)
+    term_of_value = first_term * (common_ratio ** (user_nth_term-1))
+    answer = float_to_int_converter(term_of_value)
+    sleep(0.5)
+    print(f"The value of {user_nth_term}(th/nd/st) term for this series is {answer}")
 
 def find_term(first_term, second_term, common_ratio):
     user_term_value = int(input("Enter the value of the term you want: "))
-    term_of_value = (log(user_term_value/first_term)/(log(common_ratio))) + 1
-    answer = float_to_int_converter(term_of_value)
-    print(answer)
+    term = (log(user_term_value/first_term)/(log(common_ratio))) + 1
+    answer = float_to_int_converter(term)
+    sleep(0.5)
+    print(f"The value {user_term_value} belongs to the {answer}(th/nd/st) term")
 
 def find_first_term_and_ratio():
     any_term1 = int(input("Enter the term "))
@@ -43,7 +45,8 @@ def find_first_term_and_ratio():
 
     r = int((b / c) ** (1/term_difference))
     a = int(any_term1_value / (r ** (any_term1 - 1)))
-
+    
+    sleep(0.5)
     print("First term:" + str(a))
     print("Ratio:" + str(r))
 
@@ -56,7 +59,8 @@ def find_sum(first_term, second_term, common_ratio):
         sumation = (
             first_term * ((common_ratio ** number_of_terms)-1)) / (common_ratio-1)
     answer = float_to_int_converter(sumation)
-    print(answer)
+    sleep(0.5)
+    print(f"The sum of this series is {answer}")
 
 def find_sum_to_infinity(first_term, second_term, common_ratio):
     if common_ratio > 1:
@@ -64,7 +68,8 @@ def find_sum_to_infinity(first_term, second_term, common_ratio):
     elif common_ratio < 1 and common_ratio > -1:
         sum_to_infinity = first_term / (1-common_ratio)
     answer = float_to_int_converter(sum_to_infinity)
-    print(answer)
+    sleep(0.5)
+    print(f"The sum to infinity of this series is {answer}")
 
 sleep(1)
 if user_choice != "c":

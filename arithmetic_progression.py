7345bdc44b27d0155cc4139e3788a3040d6dd295
Arafat Khan <arafat33k@outlook.com>
2019-07-22 18:07:03 +0000

    Add equation solving with sympy

--- arithmetic_progression.py
+++ arithmetic_progression.py
@@ -1,9 +1,12 @@
 from float_to_int import float_to_int_converter
 from time import sleep
+from sympy import solve
+from sympy import Eq
+from sympy import symbols
 print("This program simulates an arithmetic progression")
 
 sleep(0.5)
-user_choice = input("a) Find the value of a given term \nb) Find the term of a given value \nc) Find the sum of a series \n" )
+user_choice = input("a) Find the value of a given term \nb) Find the term of a given value \nc) Find the sum of a series \nd) Find first term and common difference" )
 
 def find_value(first_term, second_term, common_difference):
     user_nth_term = int(input("Enter the term you want the value of: "))
@@ -25,10 +28,27 @@ def find_sum(first_term, second_term, common_difference):
     sleep(0.75)
     print(f"The sum of this series is {answer}")
 
+def first_term_and_difference():
+    any_term1 = int(input("Enter any term of a series: "))
+    any_term1_value = int(input("Enter its value: "))
+    any_term2 = int(input("Enter another term of the same series: "))
+    any_term2_value = int(input("Enter its value: "))
+
+    a, d = symbols(['a', 'd'])
+    system = [
+        Eq(a + (any_term1-1)*d, any_term1_value),
+        Eq(a + (any_term2-1)*d, any_term2_value)
+        ]
+    solution = solve(system, [a, d])
+    print(f"Here, the first term is {solution[a]} and common difference is {solution[d]}")
+
+
+
 sleep(0.75)
-first_term, second_term = input("Enter the first and second term, separated by commas ").split(",")
-first_term, second_term = int(first_term), int(second_term)
-common_difference = second_term - first_term
+if user_choice != "d":
+    first_term, second_term = input("Enter the first and second term, separated by commas ").split(",")
+    first_term, second_term = int(first_term), int(second_term)
+    common_difference = second_term - first_term
 
 if user_choice == "a":
     sleep(0.75)
@@ -39,3 +59,6 @@ elif user_choice == "b":
 elif user_choice == "c":
     sleep(0.75)
     find_sum(first_term, second_term, common_difference)
+elif user_choice == "d":
+    sleep(0.75)
+    first_term_and_difference()
